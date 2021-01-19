f64887de0cf0ed5d9f0cd784297cacf0bf79866a
jianghui1103 <1219936992@qq.com>
2021-01-19 23:04:56 +0800

    writeFile 创建写入文件

--- day03/app.js
+++ day03/app.js
@@ -39,3 +39,33 @@ fs.mkdir('./css',(err)=>{
     console.log('创建成功')
 })
 */
+
+// 3. fs.writeFile 创建写入文件
+/*
+    filename    (String)            文件名称
+    data        (String | Buffer)   将要写入的内容，可以使用字符串 或 buffer 数据
+    options     (Object)            options数组对象包含
+    · encoding  (string)                可选值，默认为‘utf8’,当data使用buffer
+    · node      (Number)                文件写入权限，默认值438
+    · flag      (String)                默认值'w'
+    callback { Function } 回调，传递一个异常参数err
+*/
+
+/*
+fs.writeFile('./html/index.html','你好nodejs',(err)=>{
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log('创建写入成功');
+})
+*/
+
+// 4. fs.appendFile 追加文件
+// fs.appendFile('./css/base.css','body{color:red}',(err)=>{
+//     if(err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log('appendFile成功')
+// })
